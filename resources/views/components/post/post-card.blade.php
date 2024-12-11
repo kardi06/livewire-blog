d81fe2992701f54e99e3f6663cf2405f362cc872
kardi.ib <kardi.ib@gmail.com>
2024-12-11 16:58:28 +0700

    home blade and post-card components

---
+++ resources/views/components/post/post-card.blade.php
@@ -0,0 +1,21 @@
+@props(['post'])
+<div class="">
+    <a href="http://127.0.0.1:8000/blog/laravel-34">
+        <div>
+            <img class="w-full rounded-xl"
+                src="http://127.0.0.1:8000/storage/3i5uKG05UnvhbORZ3ieDkvtAOL8ss5-metaZXAxNSAoMjIpLnBuZw==-.png">
+        </div>
+    </a>
+    <div class="mt-3">
+        <div class="flex items-center mb-2">
+            <a href="http://127.0.0.1:8000/categories/laravel" class="bg-red-600 
+                text-white 
+                rounded-xl px-3 py-1 text-sm mr-3">
+                Laravel
+            </a>
+            <p class="text-gray-500 text-sm">2023-09-05</p>
+        </div>
+        <a class="text-xl font-bold text-gray-900">Laravel 10 tutorial feed page #34</a>
+    </div>
+
+</div>
