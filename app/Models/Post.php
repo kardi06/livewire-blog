79963c50590214616488b99c81cc822ce6ddfbd6
kardi.ib <kardi.ib@gmail.com>
2024-12-27 09:56:44 +0700

    post item components

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,11 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'published_at' => 'datetime'
+        ];
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     public function scopePublished($query)
     {
@@ -19,5 +32,15 @@ class Post extends Model
     public function scopeFeatured($query)
     {
         return $query->where('featured', true);
+    }
+
+    public function getExcerpt()
+    {
+        return STR::limit(strip_tags($this->body), 150);
+    }
+    public function getReadingTime()
+    {
+        $words = str_word_count($this->body);
+        return ceil($words / 250) < 1 ? 1 : ceil($words / 250);
     }
 }
