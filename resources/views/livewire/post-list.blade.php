72ae00a11619d973f7f1c95cff47cc367d55e59c
kardi.ib <kardi.ib@gmail.com>
2024-12-30 09:51:15 +0700

    blog sorting

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -2,8 +2,8 @@
 <div class=" px-3 lg:px-7 py-6">
     <div class="flex justify-between items-center border-b border-gray-100">
         <div class="flex items-center space-x-4 font-light ">
-            <button class="text-gray-500 py-4">Latest</button>
-            <button class="text-gray-900 py-4 border-b border-gray-700">Oldest</button>
+            <button class="{{$sort === 'desc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500'}}  py-4" wire:click="setSort('desc')">Latest</button>
+            <button class="{{$sort === 'asc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500'}} py-4 " wire:click="setSort('asc')">Oldest</button>
         </div>
     </div>
     <div class="py-4">
@@ -12,6 +12,6 @@
         @endforeach
     </div>
     <div class="my-3">
-        {{$this->posts->links()}}
+        {{$this->posts->onEachSide(1)->links()}}
     </div>
 </div>
