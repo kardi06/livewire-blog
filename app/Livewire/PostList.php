ebfd3d0f70b6be46eacd04ace02e94dac25a4001
kardi.ib <kardi.ib@gmail.com>
2025-01-01 22:30:23 +0700

    livewire search box

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Post;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Url;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -14,16 +15,23 @@ class PostList extends Component
 
     #[Url()]
     public $sort = 'desc';
+    public $search = '';
 
     public function setSort($sort)
     {
         $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
     }
 
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
+    }
+
     #[Computed()]
     public function posts()
     {
-        return Post::published()->orderBy('published_at', $this->sort)->paginate(3);
+        return Post::published()->orderBy('published_at', $this->sort)->where('title', 'like', "%{$this->search}%")->paginate(3);
     }
     public function render()
     {
