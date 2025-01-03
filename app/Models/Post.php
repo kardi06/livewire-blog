fdddbd348c5d20c1796f6baf47f57aab653e5100
kardi.ib <kardi.ib@gmail.com>
2025-01-03 14:53:32 +0700

    category filament

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
 
@@ -11,6 +12,7 @@ class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+    use SoftDeletes;
 
     protected function casts(): array
     {
